7b9da5a3a4ecb59a698879d6cffdf345bcd10663
Vishavpreet Sandhu <vishal1949@gmail.com>
2019-04-08 13:54:52 -0700

    Connect to mongo

--- app.js
+++ app.js
@@ -1,39 +1,18 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
 const path = require('path')
 const fetch = require('node-fetch')
 const PORT = process.env.PORT || 8000; // process.env accesses heroku's environment variables
+const db = require('./config/keys').mongoURI;
 
-app.use(express.static('public'))
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
-app.get('/', (request, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'))
-})
+app.get("/", (req, res) => res.send("Hello World"));
 
-// create route to get single book by its isbn
-app.get('/books/:isbn', (request, response) => {
-  // make api call using fetch
-  fetch(`http://openlibrary.org/api/books?bibkeys=ISBN:${request.params.isbn}&format=json&jscmd=data`)
-  .then((response) => {
-      return response.text();
-  }).then((body) => {
-      let results = JSON.parse(body)
-      console.log(results)   // logs to server
-      response.send(results) // sends to frontend
-    });
-});
-
-// create a search route
-app.get('/search', (request, response) => {
-  fetch(`http://openlibrary.org/search.json?q=${request.query.string}`)
-  .then((response) => {
-      return response.text();
-  }).then((body) => {
-      let results = JSON.parse(body)
-      console.log(results)
-      response.send(results)
-    });
-});
 
 app.listen(PORT, () => {
   console.log(__dirname);
